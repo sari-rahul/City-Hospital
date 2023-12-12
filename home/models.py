08cb20bb9fab4b41021511e4fec5f72d2804f567
sari-rahul <>
2023-12-12 16:34:54 +0000

    create model Doctors

--- home/models.py
+++ home/models.py
@@ -7,3 +7,9 @@ class Departments(models.Model):
     dep_name = models.CharField(max_length=100)
     dep_description = models.TextField()
     
+
+class Doctors(models.Model):
+    doc_name = models.CharField(max_length=255)
+    doc_spec = models.CharField(max_length=100)
+    dep_name = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    doc_image = models.ImageField(upload_to = 'doctors')
