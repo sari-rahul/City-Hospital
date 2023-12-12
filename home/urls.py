c40f03c28182b93e83ed16d6ffb02a0a13fe1dd1
sari-rahul <>
2023-12-12 08:58:51 +0000

    Create superuser

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('booking', views.booking),
-    path('doctors', views.doctors),
-    path('contact', views.contact),
-    path('department', views.department),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('booking', views.booking, name='booking'),
+    path('doctors', views.doctors, name='doctors'),
+    path('contact', views.contact, name='contact'),
+    path('department', views.department, name='departments'),
 ]
