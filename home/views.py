6e2463b35339188aba5f92fe2f7fcfb3476dcce1
sari-rahul <>
2023-12-12 08:16:38 +0000

    Create basic Templates for all Pages

--- home/views.py
+++ home/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def booking(request):
+    return render(request, 'booking.html')
+
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def department(request):
+    return render(request, 'department.html')
