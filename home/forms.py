ba300fd3622e06cfbfdf2d5a373846cb56706aa5
sari-rahul <>
2023-12-13 11:05:30 +0000

    Create Forms.py for Forms in Booking page

---
+++ home/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Bookings
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Bookings
+        fields = '__all__'
+        widgets = {
+            'booking_date' : DateInput()
+        }
