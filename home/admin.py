25aaea7aee1dff45bfa941c06b48cae7d24d0013
sari-rahul <>
2023-12-13 14:36:46 +0000

    display  booking info in table format in database

--- home/admin.py
+++ home/admin.py
@@ -5,5 +5,13 @@ from .models import Departments, Doctors, Bookings
 
 admin.site.register(Departments)
 admin.site.register(Doctors)
-admin.site.register(Bookings)
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'p_name', 'p_phone', 'p_email', 'doc_name',
+                    'booking_date', 'booked_on')
+
+
+admin.site.register(Bookings,  BookingAdmin)
+
 
